54bd67fcfeaf967590b700dd45bd0195903d0e8b
ayush-oberoi <ayushoberoi55@gmail.com>
2020-12-18 09:08:14 +0530

    One line function reverse

--- Arrays/reverse-string.py
+++ Arrays/reverse-string.py
@@ -3,4 +3,7 @@ def reverse(myarray):
 	for i in range(len(myarray)-1,-1,-1):
 		l.append(myarray[i])
 	return ''.join(l)
+def reverse1(array):
+	return array[::-1]
 print(reverse("Azure Cloud is Awesome"))
+print(reverse1("Google Cloud is Awesome"))
